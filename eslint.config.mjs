24f4a93d7ae9f46b40201657b32db6dcc5a8e57c
SuhEugene <suheugene@ya.ru>
2025-06-27 00:15:39 +0300

    Eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    'vue/max-attributes-per-line': 'off',
+    '@stylistic/max-statements-per-line': ['error', { max: 2 }],
+    '@stylistic/semi': ['error', 'always'],
+    'vue/multi-word-component-names': 'off',
+  },
+});
