f1749780ed8a683a528a31291edc625499df379b
SuhEugene <suheugene@ya.ru>
2025-06-27 01:05:13 +0300

    Initialize tailwind and stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon'],
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon']
-})
+  css: ['~/app/assets/css/tailwind.css'],
+  compatibilityDate: '2025-05-15',
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+});
