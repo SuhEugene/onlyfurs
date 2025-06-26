eb297be17ea1d26569f0f42b97170ee225a47e97
SuhEugene <suheugene@ya.ru>
2025-06-27 02:01:16 +0300

    Add dark theme and meta

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,41 @@
+<script lang="ts" setup>
+useHead({
+  title: 'Nuxt 3 Minimal Starter',
+  htmlAttrs: {
+    lang: 'ru',
+    class: 'dark',
+  },
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' },
+  ],
+});
+</script>
+
 <template>
-  <div>
+  <div class="min-h-screen">
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </div>
 </template>
+
+<style>
+body {
+  width: 100%;
+  overflow-y: auto;
+  overflow-x: hidden;
+  scrollbar-gutter: stable;
+}
+
+#__nuxt {
+  display: flex;
+  flex: 1 0 auto;
+  min-height: 100%;
+  width: 100%;
+}
+
+* {
+  scrollbar-width: thin;
+  scrollbar-color: var(--border) var(--color-background);
+}
+</style>
