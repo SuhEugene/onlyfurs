ca3be04f2d9f6c1801d4e5d6497bcd878c8d51ab
SuhEugene <suheugene@ya.ru>
2025-07-05 02:01:33 +0300

    Add Join button

--- app/components/InfoBar.vue
+++ app/components/InfoBar.vue
@@ -4,7 +4,8 @@
 
 <template>
   <div class="flex flex-col py-5 pl-7 w-[328px] fixed infobar">
-    <div class="w-full h-20 rounded-md bg-muted" />
+    <!-- <div class="w-full h-20 rounded-md bg-muted" /> -->
+     <InfoBarJoin />
     <div class="w-full border-t border-muted my-4" />
     <div class="flex flex-row justify-center  flex-wrap gap-2">
       <InfoBarLink label="Privacy Policy" to="/privacy-policy" />
---
+++ app/components/InfoBarJoin.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2 class="text-xl font-extrabold mb-4">
+    Присоединиться к сообществу
+  </h2>
+  <button class="px-4 py-2 flex flex-row items-center justify-center rounded-md bg-primary hover:bg-primary/80 cursor-pointer transition-colors duration-100">
+    Зарегистрироваться
+  </button>
+</template>
