4669c8c975a53d363c7ff3ab457efebc63e676c5
SuhEugene <suheugene@ya.ru>
2025-07-05 05:55:22 +0300

    Add main page navbar highlight

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -1,12 +1,28 @@
 <script lang="ts" setup>
+const route = useRoute();
+const isMainPage = computed(() => route.path === '/');
 </script>
 
 <template>
   <div class="flex flex-col py-2.5 px-5 w-60 fixed navbar">
     <NavBarProfile />
-    <NavBarItem label="Главная" active-icon="mingcute:home-4-fill" inactive-icon="mingcute:home-4-line" is-active />
-    <NavBarItem label="Explore" active-icon="mingcute:search-fill" inactive-icon="mingcute:search-line" />
-    <NavBarItem label="Уведомления" active-icon="mingcute:notification-fill" inactive-icon="mingcute:notification-line" />
+    <NavBarItem
+      label="Главная"
+      active-icon="mingcute:home-4-fill"
+      inactive-icon="mingcute:home-4-line"
+      :is-active="isMainPage"
+      @click="$router.push('/')"
+    />
+    <NavBarItem
+      label="Поиск"
+      active-icon="mingcute:search-fill"
+      inactive-icon="mingcute:search-line"
+    />
+    <NavBarItem
+      label="Больше"
+      active-icon="mingcute:more-3-fill"
+      inactive-icon="mingcute:more-3-line"
+    />
   </div>
 </template>
 
