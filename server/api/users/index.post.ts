0e6441603948897bb008beb49515601af059608e
SuhEugene <seapps.tk@ya.ru>
2026-02-25 04:59:50 +0300

    Log user creation steps

--- server/api/users/index.post.ts
+++ server/api/users/index.post.ts
@@ -1,6 +1,7 @@
 export default defineEventHandler(async (event) => {
   checkToken(event);
 
+  console.log('Requested user creation, reading body...');
   const body = await readMultipartFormData(event);
   const userData = {
     handle: String(body?.find((el) => el.name === 'handle')?.data),
@@ -11,16 +12,21 @@ export default defineEventHandler(async (event) => {
     banner: body?.find((el) => el.name === 'banner'),
   };
 
+  console.log('Body read, validating...');
   if (!userData.handle) throw createError({ status: 400 });
   if (!userData.username) throw createError({ status: 400 });
   if (!userData.avatar) throw createError({ status: 400 });
   if (!userData.banner) throw createError({ status: 400 });
 
+  console.log('Establishing connections...');
   const s3Client = useS3Mini();
   const db = useDrizzle();
+
+  console.log('Initiating transaction...');
   const error = await db
     .transaction(
       async (tx) => {
+        console.log('Creating user...');
         const [user] = await tx
           .insert(tables.users)
           .values({
@@ -33,6 +39,7 @@ export default defineEventHandler(async (event) => {
 
         if (!user) return tx.rollback();
 
+        console.log('Uploading avatar and banner...');
         const { avatar, banner } = userData;
         const avatarUploadP = s3Client.putAnyObject(`ava/${user.id}`, avatar!.data, avatar!.type);
         const bannerUploadP = s3Client.putAnyObject(`bnr/${user.id}`, banner!.data, banner!.type);
@@ -48,20 +55,25 @@ export default defineEventHandler(async (event) => {
           return tx.rollback();
         }
 
+        console.log('Checking avatar and banner...');
         if (!avatarUpload.ok) return rollbackFiles();
         if (!bannerUpload.ok) return rollbackFiles();
 
-        await tx.transaction(async (tx) => {
-          await tx.update(tables.users)
-            .set({
-              avatarURL: `ava/${user.id}`,
-              bannerURL: `bnr/${user.id}`,
-            })
-            .where(eq(tables.users.id, user.id));
-        }).catch((error) => {
-          console.error(error);
-          return rollbackFiles();
-        });
+        console.log('Updating avatar and banner...');
+        await tx
+          .transaction(async (tx) => {
+            await tx
+              .update(tables.users)
+              .set({
+                avatarURL: `ava/${user.id}`,
+                bannerURL: `bnr/${user.id}`,
+              })
+              .where(eq(tables.users.id, user.id));
+          })
+          .catch((error) => {
+            console.error(error);
+            return rollbackFiles();
+          });
       },
       { accessMode: 'read write' },
     )
@@ -72,5 +84,6 @@ export default defineEventHandler(async (event) => {
 
   if (error) throw createError({ status: 500 });
 
+  console.log("User created, done!");
   return userData.handle;
 });
