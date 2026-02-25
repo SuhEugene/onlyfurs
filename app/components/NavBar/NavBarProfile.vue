0d90bd634418cc1f99014d3fdf0e0a2a957f5f12
SuhEugene <seapps.tk@ya.ru>
2026-02-25 06:27:34 +0300

    Add trackedOpen to anon user avatar

--- app/components/NavBar/NavBarProfile.vue
+++ app/components/NavBar/NavBarProfile.vue
@@ -1,5 +1,11 @@
+<script lang="ts" setup>
+const { trackedOpen } = useRegistration();
+</script>
 <template>
-  <button class="my-3 xl:pl-4 flex items-center justify-center md:justify-start">
+  <button
+    class="my-3 xl:pl-4 flex items-center justify-center md:justify-start"
+    @click="trackedOpen('navbar-avatar')"
+  >
     <div
       class="size-8 md:size-12 flex justify-center items-center rounded-full bg-gray-600 text-white cursor-pointer transition-all border-2 border-transparent hover:border-primary/70 hover:outline-8 hover:outline-primary/40"
     >
--- app/composables/registration.ts
+++ app/composables/registration.ts
@@ -1,7 +1,15 @@
 import { useModal } from 'vue-final-modal';
 import ModalRegistration from '~/components/ModalRegistration.vue';
 
-type ClickSource = 'post-buttons' | 'post-image' | 'subscription' | 'registration-cta' | 'author-follow' | 'author-more' | 'navbar';
+type ClickSource =
+  | 'post-buttons'
+  | 'post-image'
+  | 'subscription'
+  | 'registration-cta'
+  | 'author-follow'
+  | 'author-more'
+  | 'navbar'
+  | 'navbar-avatar';
 
 export const useRegistration = () => {
   const { open, close } = useModal({
@@ -19,6 +27,6 @@ export const useRegistration = () => {
   const trackedOpen = (source: ClickSource) => {
     umTrackEvent(`registration.open`, { clicked: source });
     open();
-  }
+  };
   return { trackedOpen, close };
 };
