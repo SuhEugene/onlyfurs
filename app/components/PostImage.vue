1411275a626bad4a69d5b99061b57441f6f4fc4b
SuhEugene <seapps.tk@ya.ru>
2026-02-25 06:36:07 +0300

    Post image hover blur

--- app/components/PostImage.vue
+++ app/components/PostImage.vue
@@ -22,10 +22,15 @@ const { trackedOpen } = useRegistration();
     @click.prevent.stop="() => trackedOpen('post-image')"
   >
     <div
-      class="absolute inset-0 opacity-0 group-hover/post-image:opacity-100 bg-background/40 transition-all flex flex-row items-center justify-center gap-2 z-2"
+      :class="
+        cn(
+          'absolute inset-0 opacity-0 group-hover/post-image:opacity-100 bg-background/40 transition-all flex flex-row items-center justify-center gap-2 z-2 text-shadow-lg',
+          cropped && 'backdrop-blur-xs bg-background/50',
+        )
+      "
     >
       <Icon name="mingcute:eye-line" :size="24" />
-      <span>Показать изображение</span>
+      <span>Показать {{ cropped ? 'полностью' : 'изображение' }}</span>
     </div>
     <div
       v-if="cropped"
