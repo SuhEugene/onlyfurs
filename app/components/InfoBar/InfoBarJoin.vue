9c83f795e0b860db657c1b70e924ffe50c4d2bf7
SuhEugene <seapps.tk@ya.ru>
2026-03-24 00:54:21 +0300

    Nicer CTA button

--- app/components/InfoBar/InfoBarJoin.vue
+++ app/components/InfoBar/InfoBarJoin.vue
@@ -5,7 +5,7 @@ const { trackedOpen } = useRegistration();
 <template>
   <h2 class="text-xl font-extrabold mb-4">Присоединиться к сообществу</h2>
   <button
-    class="px-4 py-2 flex flex-row items-center justify-center rounded-md bg-primary hover:bg-primary-hover cursor-pointer transition-colors duration-100"
+    class="px-4 py-2 w-full flex flex-row items-center justify-center rounded-md bg-primary hover:bg-primary-hover cursor-pointer transition-colors duration-100"
     @click="trackedOpen('registration-cta')"
   >
     Зарегистрироваться
